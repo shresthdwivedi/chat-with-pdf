d5fc1daf8034d40965fa07bd0ad02972fb2bfd61
Shresth Dwivedi <shresth0109@gmail.com>
2025-03-17 13:20:45 +0530

    update config

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,5 +1,5 @@
 export const indexName = "index-one"
-export const namespace = "diagnosis2"
+export const namespace = "testspace"
 export const topK = 5
 export const modelname = 'mixedbread-ai/mxbai-embed-large-v1'
 
